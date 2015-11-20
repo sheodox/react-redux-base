41dbca098c027285626a15ff948e2719020ed3d0
sheodox <imagamer27@msn.com>
2015-11-19 20:14:21 -0600

    actually attach react on the client

--- src/client/client.js
+++ src/client/client.js
@@ -1,1 +1,12 @@
 import React from 'react';
+import {render} from 'react-dom';
+import {Router} from 'react-router';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
+import routes from '../shared/router';
+
+const history = createBrowserHistory();
+
+render(
+    <Router children={routes} history={history} />,
+    document.getElementById('react')
+);
