2aff13a6cab87bacbc4dbfa083b74f2c4ee39266
sheodox <imagamer27@msn.com>
2015-11-22 20:32:57 -0600

    added redux needs, and connect to react-redux

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,12 +1,19 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import * as actions from '../actions/actions';
 
+@connect (state => ({greeting: state.greeting}))
 class App extends Component {
+    static needs = [
+        actions.getGreeting
+    ];
+
     constructor(props) {
         super(props);
     }
     render() {
         return (
-            <h1>hello, world!</h1>
+            <h1>{this.props.greeting}</h1>
         );
     }
 }
