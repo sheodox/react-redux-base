87c9372239076acb9420c493f346cf622c10e694
sheodox <imagamer27@msn.com>
2015-11-22 22:11:39 -0600

    created a 'components' directory

--- src/shared/routes.js
+++ src/shared/routes.js
@@ -1,7 +1,7 @@
 'use strict';
 import React from 'react';
 import {Router, Route, Link} from 'react-router';
-import App from './App';
+import App from './components/App';
 
 export default [{
     path: '/',
