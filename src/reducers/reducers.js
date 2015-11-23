c70935a3cb7313d32f230f5018bccd4b567f9588
sheodox <imagamer27@msn.com>
2015-11-22 22:00:33 -0600

    simplify the export

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,7 +1,7 @@
 import {combineReducers} from 'redux';
 
-import greetingReducer from './greetingReducer';
+import greeting from './greetingReducer';
 
 export default combineReducers({
-    greeting: greetingReducer
+    greeting
 });
