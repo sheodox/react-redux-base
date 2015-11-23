390cf6465f5ad8eb84d8baf8868f3e3d654e81f6
sheodox <imagamer27@msn.com>
2015-11-22 20:31:38 -0600

    return the entire 'hello, world!' phrase

--- src/api/hello.js
+++ src/api/hello.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('world!');
+    res.send('Hello, world!');
 });
 
 export default router;
