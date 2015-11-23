2d0281e1c685dc0e2d0e114cc87c8f08f57c4c3f
sheodox <imagamer27@msn.com>
2015-11-22 22:07:18 -0600

    initialize redux on the client

--- src/client/client.js
+++ src/client/client.js
@@ -2,11 +2,18 @@ import React from 'react';
 import {render} from 'react-dom';
 import {Router} from 'react-router';
 import {Provider} from 'react-redux';
+import {fromJS} from 'immutable';
 import createStoreWithMiddleware from '../middleware/middleware';
+import reducers from '../reducers/reducers';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
 import routes from '../shared/routes';
+import immutify from './immutify';
 
 const history = createBrowserHistory();
+
+const initialState = immutify(__INITIAL__);
+
+const store = createStoreWithMiddleware(reducers, initialState);
 
 render(
     <Provider store={store}>
